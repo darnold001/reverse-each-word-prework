ab07b5123f75d8f04617b35ebc7b74a8f22280e6
Devin Arnold <dbarnold321@gmail.com>
2019-05-12 15:45:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(text)
   #code
   new_array = []
   array = text.split(" ")
-  array.each do |word|
+  array.collect do |word|
   reversed =  word.reverse
   new_array.push (reversed)
 end
