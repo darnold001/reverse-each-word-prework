3b2a2e4f691688b547712283f4653b147f1f69e1
Devin Arnold <dbarnold321@gmail.com>
2019-05-12 16:30:14 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(text)
+  #code
+  new_array = []
+  array = text.split(" ")
+  array.collect do |word|
+  reversed =  word.reverse
+  new_array.push (reversed)
+end
+new_array.join(" ")
+end
